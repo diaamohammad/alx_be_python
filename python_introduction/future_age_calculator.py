2dc9ffd80a7946797e64e4d9131aadecdd106351
diaa mohammad <diaamohammad078@gmail.com>
2024-09-12 14:03:32 +0300

    Age Calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,16 @@
+#ask age
+
+age =int(input("how old are you ?\n"))
+
+#add his age 
+
+future_age = age + 27 
+
+#print his future age
+
+print(f"In 2050, you will be {future_age} years old")
+
+
+
+
+
