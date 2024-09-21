1c36302df8a13ccbed442dd1cb920f6962cebd33
diaa mohammad <diaamohammad078@gmail.com>
2024-09-21 15:55:45 +0300

    Multiplication Table Generator

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+#Prompt User for a Number:
+
+number= int(input("Enter a number to see its multiplication table:"))
+
+for y in range(1,11):
+    z = number * y
+    print(f"{number} * {y} ={z}")
+
+
