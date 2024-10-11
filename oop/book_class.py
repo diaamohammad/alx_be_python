c2cbe1dbb31fd54b15a5f0fab4c382a976b3000e
diaa mohammad <diaamohammad078@gmail.com>
2024-10-11 23:44:53 +0300

    Magic Methods

---
+++ oop/book_class.py
@@ -0,0 +1,22 @@
+class Book:
+
+    def __init__(self,title,author,year):
+
+        self.title=title
+        self.author=author
+
+        if isinstance(year,int):
+
+            self.year=year
+
+        else:
+            raise ValueError
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    def __del__(self):
+        print(f"Deleting {self.title}")
